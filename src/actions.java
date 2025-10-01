fc8cb1c7aaf36646e62b589e14f8630c7b6e3716
jaminkar <harshct9601@gmail.com>
2025-10-01 14:34:26 +0530

    from user1

--- src/actions.java
+++ src/actions.java
@@ -21,7 +21,8 @@ public class actions {
 		a.moveToElement(driver.findElement(By.xpath("(//a[@class='nav-a nav-a-2   nav-progressive-attribute'])[1]"))).build().perform();
 		driver.manage().window().maximize();
 		a.moveToElement(driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
-		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
 		
 		/*
